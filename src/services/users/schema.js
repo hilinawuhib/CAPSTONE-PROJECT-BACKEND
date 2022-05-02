e594843427ad9afd07af47848e230bd5d985977e
hilina88 <hilinawuhib@gmail.com>
2022-05-02 21:31:05 +0200

    Update schema.js

--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -3,8 +3,8 @@ import bcrypt from "bcrypt";
 const { Schema, model } = mongoose;
 const userSchema = new Schema(
   {
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String },
     role: { type: String, enum: ["parent", "Tutor"], default: "parent" },
